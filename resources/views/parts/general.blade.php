97d152030189eb0d0f7da52d13e47d799c76e360
George Grammenidis <grammenidis@yahoo.gr>
2019-02-05 16:47:29 +0200

    Created basic form for general without any validation

--- resources/views/parts/general.blade.php
+++ resources/views/parts/general.blade.php
@@ -13,4 +13,31 @@
       </ul>
     </div>
   </div>
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-3">
+      <label for="title">Title</label>
+      <select id="title" class="form-control">
+        <option selected>Mr</option>
+        <option>Mrs</option>
+        <option>Ms</option>
+        <option>Miss</option>
+        <option>Mx</option>
+      </select>
+    </div>
+    <div class="form-group col-md-9">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" placeholder="Type your name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="age">Age</label>
+    <input type="number" class="form-control" id="age" placeholder="Type your age">
+  </div>
+  <div class="form-group">
+    <label for="country">Country</label>
+    <input type="number" class="form-control" id="country" placeholder="Type your country">
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
 @endsection
