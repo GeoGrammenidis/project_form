a57eed587dd38c5dafd67124a0878913de67ff32
George Grammenidis <grammenidis@yahoo.gr>
2019-02-06 21:43:25 +0200

    Added menu for easier navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,42 @@
         <title>Questionnaire</title>
     </head>
     <body>
+
         <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/questionnaire">Questionnaire</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item @if (Request::is('questionnaire'))
+                    active
+                @endif">
+                    <a class="nav-link" href="/questionnaire">Home
+                        @if (Request::is('questionnaire'))
+                            <span class="sr-only">(current)</span>
+                        @endif</a>
+                </li>
+                <li class="nav-item @if (Request::is('questionnaire/create'))
+                    active
+                @endif">
+                    <a class="nav-link" href="/questionnaire/create" >Create
+                        @if (Request::is('questionnaire/create'))
+                            <span class="sr-only">(current)</span>
+                        @endif</a>
+                </li>
+                <li class="nav-item @if (Request::is('questionnaire/edit'))
+                    active
+                @endif">
+                    <a class="nav-link" href="/questionnaire/edit">Edit/Delete
+                        @if (Request::is('questionnaire/edit'))
+                            <span class="sr-only">(current)</span>
+                        @endif</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
         @yield('content')
         </div>
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
