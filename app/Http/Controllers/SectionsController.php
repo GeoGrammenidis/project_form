76904e3e655c78d3385273f9731212ea14fd95f5
George Grammenidis <grammenidis@yahoo.gr>
2019-02-06 17:10:02 +0200

    made controllers readable/moved staff to class methods

--- app/Http/Controllers/SectionsController.php
+++ app/Http/Controllers/SectionsController.php
@@ -6,36 +6,21 @@ use Illuminate\Http\Request;
 use App\Section;
 use App\Question;
 use App\Option;
-use App\Answer;
-class SectionsController extends Controller
-{
+
+class SectionsController extends Controller{
     public function index(){
     	$sections = Section::all();
     	$questions = Question::all();
     	$options = Option::all();
-    	// methode to be called from model for order.
-    	return view('contents/index', compact('sections', 'questions', 'options'));
+    	return view('contents/index',
+            compact('sections', 'questions', 'options')
+        );
     }
 
     public function store(Section $section){
-    	$section_questions = Question::where('section_id',$section->id)->get();
-    	$data = [];
-    	foreach ($section_questions as $section_question) {
-    		if($section_question->input_type=='text')
-    			$val = ['required','min:3','max:255'];
-    		else if($section_question->input_type=='number')
-    			$val = ['required','numeric'];
-    		else
-    			$val = ['required'];
-    		$data['question_'.$section_question->id]=$val;
-    	}
+        $data = $section->get_table_to_val($section);
     	request()->validate($data);
-    	foreach ($section_questions as $section_question) {
-    		if(!is_array(request()->all()['question_'.$section_question->id]))
-    			Answer::create(['question_id'=>$section_question->id,"value"=>request()->all()['question_'.$section_question->id]]);
-    		else
-    			Answer::create(['question_id'=>$section_question->id,"value"=>implode(", ",request()->all()['question_'.$section_question->id])]);
-    	}
+        $section->create_answers($section, $data);
     	return redirect('/questionnaire');
     }
 }
